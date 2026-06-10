972601f396485965db58b7b040f9f84dc3340df4
dhoat30 <dhoat30@gmail.com>
2026-06-10 15:23:43 +1200

    added seo schema

--- Components/UI/Forms/GetQuoteForm.js
+++ Components/UI/Forms/GetQuoteForm.js
@@ -267,7 +267,7 @@ export default function GetQuoteForm({
                 variant="contained"
                 className="mt-16 full-width"
               >
-                Get Free Moving Quote
+                Get Free Quote
               </Button>
               <Button
                 variant="text"
